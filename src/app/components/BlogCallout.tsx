79a4fe7267de14925a18f067047d3a435112d4a6
Colin Hemphill <colin.hemphill@bit.ly>
2023-04-14 23:12:37 -0500

    Remove blogcallout animations.

--- src/app/components/BlogCallout.tsx
+++ src/app/components/BlogCallout.tsx
@@ -1,39 +1,22 @@
-'use client';
-
 import ButtonLink from '@/strum/ButtonLink';
 import HeadingWithIcon from '@/strum/HeadingWithIcon';
-import useInViewAnimate from '@/utils/useInViewAnimate';
 import { ArrowRight, Rss } from 'lucide-react';
-import { twMerge } from 'tailwind-merge';
 
 export default function BlogCallout() {
-  const { staggerClassName, viewRef } = useInViewAnimate(
-    'blog-callout',
-    'fast',
-  );
-
   return (
-    <aside className="mt-24 text-center md:mt-32" ref={viewRef}>
+    <aside className="mt-24 text-center md:mt-32">
       <HeadingWithIcon
-        className={twMerge(
-          'items-center justify-center text-center text-4xl md:text-5xl',
-          staggerClassName,
-        )}
+        className="items-center justify-center text-center text-4xl md:text-5xl"
         Icon={Rss}
         level={2}
         text="Colin’s Blog"
       />
-      <p
-        className={twMerge(
-          'mx-auto mb-6 mt-4 max-w-3xl text-xl font-semibold md:text-2xl',
-          staggerClassName,
-        )}
-      >
+      <p className="mx-auto mb-6 mt-4 max-w-3xl text-xl font-semibold md:text-2xl">
         Read up on Colin’s explorations of front end web development, music
         production, audio engineering, and podcasting.
       </p>
 
-      <ButtonLink className={staggerClassName} href="/blog" size="xl">
+      <ButtonLink href="/blog" size="xl">
         Blog <ArrowRight size="1em" />
       </ButtonLink>
     </aside>
