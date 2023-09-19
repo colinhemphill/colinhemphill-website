c4ddbc5a2d301d55290e924ad7b3abab45385614
Colin Hemphill <colin.hemphill@bit.ly>
2023-09-19 15:47:03 -0500

    Upgrades.

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import { withContentlayer } from 'next-contentlayer';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     mdxRs: true,
   },
   images: {
