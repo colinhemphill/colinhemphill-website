15b9d945a46762d0cc45b33a2b06f6ecf14bba79
Colin Hemphill <colin.hemphill@bit.ly>
2024-11-08 18:05:08 -0600

    Lint config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ import { withContentlayer } from 'next-contentlayer2';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     mdxRs: true,
   },
