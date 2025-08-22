fdbfa7f7a7afc256f52df62eb66ffbcef1384b65
Colin Hemphill <colin.hemphill@bit.ly>
2025-08-21 20:31:48 -0500

    Lint fixes

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -4,38 +4,25 @@ import Alert from '@/strum/Alert';
 import Button from '@/strum/Button';
 import Input from '@/strum/Input';
 import Textarea from '@/strum/Textarea';
-import { FormEventHandler, useRef, useState } from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
 export default function ContactForm() {
-  const formRef = useRef<HTMLFormElement>(null);
-  const [error, setError] = useState('');
-  const [message, setMessage] = useState('');
-  const [submitting, setSubmitting] = useState(false);
+  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORMSPREE_ID!);
 
-  const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-    setError('');
-    setMessage('');
-
-    e.preventDefault();
-    setSubmitting(true);
-
-    const formData = new FormData(formRef.current as HTMLFormElement);
-
-    try {
-      await fetch('/__forms.html', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: new URLSearchParams(formData as any).toString(),
-      });
-      setMessage('Thanks, your message was submitted!');
-      formRef.current?.reset();
-    } catch (err) {
-      const error = err as Error;
-      setError(error.message);
-    } finally {
-      setSubmitting(false);
-    }
-  };
+  if (state.succeeded) {
+    return (
+      <>
+        <p className="text-neutral-11 mt-4">
+          Colin is pretty busy, but if you’re not spam he will do his best to
+          get back to you! Please <em>do not reach out</em> if you’re recruiting
+          for opportunities in Web3, crypto, AI/ML, or fintech.
+        </p>
+        <Alert color="primary" className="mt-8">
+          Thanks, your message was submitted!
+        </Alert>
+      </>
+    );
+  }
 
   return (
     <>
@@ -45,23 +32,7 @@ export default function ContactForm() {
         opportunities in Web3, crypto, AI/ML, or fintech.
       </p>
 
-      <form
-        aria-describedby={message ? 'message' : undefined}
-        aria-errormessage={error ? 'error' : undefined}
-        className="mt-8 flex flex-col gap-6"
-        data-netlify="true"
-        name="contact"
-        onSubmit={onSubmit}
-        ref={formRef}
-      >
-        <input type="hidden" name="form-name" value="contact" />
-
-        <p className="hidden">
-          <label>
-            Don’t fill this out if you’re human: <input name="bot-field" />
-          </label>
-        </p>
-
+      <form className="mt-8 flex flex-col gap-6" onSubmit={handleSubmit}>
         <Input
           errors={[]}
           label="Name"
@@ -70,6 +41,7 @@ export default function ContactForm() {
           required
           type="text"
         />
+        <ValidationError prefix="Name" field="name" errors={state.errors} />
 
         <Input
           errors={[]}
@@ -79,6 +51,7 @@ export default function ContactForm() {
           required
           type="email"
         />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
 
         <Textarea
           errors={[]}
@@ -88,20 +61,20 @@ export default function ContactForm() {
           placeholder="Your message"
           required
         />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
 
-        {error && (
-          <Alert color="warning" id="error">
-            {error}
+        {state.errors && Object.keys(state.errors).length > 0 && (
+          <Alert color="warning">
+            There was an error submitting your form. Please try again.
           </Alert>
         )}
-        {message && (
-          <Alert color="primary" id="message">
-            {message}
-          </Alert>
-        )}
 
-        <Button className="self-end" disabled={submitting} type="submit">
-          Submit
+        <Button className="self-end" disabled={state.submitting} type="submit">
+          {state.submitting ? 'Submitting...' : 'Submit'}
         </Button>
       </form>
     </>
